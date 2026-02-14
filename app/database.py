c09ab1e8a3e3fd188d1c7bd57885ed8499ef9750
MADHESH-28 <madheshdhamodharan@gmail.com>
2026-02-14 19:34:34 +0530

    sqlite_update

--- app/database.py
+++ app/database.py
@@ -1,19 +1,100 @@
-import json
+# import json
+#
+#
+# shipments={}
+# print("before load:",shipments)
+#
+# with open("shipments.json") as json_file:
+#     data = json.load(json_file)
+#
+#     for value in data:
+#         shipments[value["id"]] = value
+#
+# print("after load:",shipments)
+#
+# def save():
+#     with open("shipments.json","w") as json_file:
+#         json.dump(list(shipments.values()),json_file)
+#
+import sqlite3
+from typing import Any
+
+from schemas import ShipmentCreate, ShipmentUpdate
 
 
-shipments={}
-print("before load:",shipments)
+class DataBase:
+    def __init__(self):
+        self.conn = sqlite3.connect("sqlite.db",check_same_thread=False)
+        self.cur = self.conn.cursor()
+        self.create_table()
 
-with open("shipments.json") as json_file:
-    data = json.load(json_file)
+    def create_table(self):
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS shipment (
+            id INTEGER PRIMARY KEY,
+            content TEXT,
+            weight REAL,
+            status TEXT
+            )
+        """,)
 
-    for value in data:
-        shipments[value["id"]] = value
+    def create(self,shipment:ShipmentCreate):
+        self.cur.execute("SELECT MAX(id) FROM shipment ")
+        result = self.cur.fetchone()
 
-print("after load:",shipments)
+        new_id = result[0] + 1
 
-def save():
-    with open("shipments.json","w") as json_file:
-        json.dump(list(shipments.values()),json_file)
+        #insert values in the table
+        self.cur.execute("""
+            INSERT INTO shipment
+            VALUES (:id, :content, :weight, :status)
+        """,
+            {
+                "id": new_id,
+                **shipment.model_dump(),
+                "status":"placed"
+            }
+        )
+        self.conn.commit()
+        return new_id
 
+    def get(self,id:int) -> dict[str, Any] | None:
+        self.cur.execute("""
+            SELECT * FROM shipment
+            WHERE id =?
+         """ ,(id,))
+        row = self.cur.fetchone()
+        return {
+            "id":row[0],
+            "content":row[1],
+            "weight":row[2],
+            "status":row[3],
+        }if row else None
+
+    def update(self,id:int,shipment:ShipmentUpdate):
+        # 4. update a value
+        self.cur.execute("""
+            UPDATE shipment SET status = :status
+            WHERE id = :id
+        """,
+ {
+                "id":id,
+                **shipment.model_dump()
+            }
+        )
+        self.conn.commit()
+
+        return self.get(id)
+
+
+    def delete(self,id:int) :
+        self.cur.execute("""
+            DELETE FROM shipment
+            WHERE id = ?
+            """, (id,))
+        self.conn.commit()
+
+
+    def close(self):
+        self.conn.close()
 
--- app/main.py
+++ app/main.py
@@ -1,91 +1,65 @@
 
 from fastapi import FastAPI,HTTPException,status
+from scalar_fastapi import get_scalar_api_reference
 from typing import Any
-from database import shipments,save
+from database import DataBase
 
 from schemas import ShipmentRead, ShipmentCreate, ShipmentUpdate
 
 app = FastAPI()
+db = DataBase()
 
-
-# shipments = {
-#     12701:{
-#         "weight": .6,
-#         "content": "glasswar",
-#         "status":"shipping"
-#     },
-#     12702: {
-#                "weight": .10,
-#                "content": "table",
-#                "status": "placed"
-#     },
-#     12703: {
-#         "weight": 20,
-#         "content": "cloths",
-#         "status": "placed"
-#     },
-#     12704: {
-#                "weight": .11,
-#                "content": "wood",
-#                "status": "placed"
-#     }
-# }
 @app.get("/shipment",response_model=ShipmentRead)
 def get_shipment(id :int):
-    if id not in shipments:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="given id does not exist")
+    shipment = db.get(id)
+    if shipment is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="given id does not exist",
+        )
 
-    return shipments[id]
+    return shipment
 
 @app.post("/shipment",response_model=None)
 def submit_shipment(shipment:ShipmentCreate) -> dict[str, int]:
-    new_id=max(shipments.keys()) +1
+    new_id= db.create(shipment)
+    return {"id":new_id}
 
-    shipments[new_id] = {
-        **shipment.model_dump(),
-        "id": new_id,
-        "status": "shipping"
-    }
-    save()
-    return {"id": new_id}
-
-@app.get("/shipment/{id}")
-def get_shipment(id:int) -> dict[str, Any]:
-
-    if id not in shipments:
-        return {"detail" : "Given id doesn't exist"}
-
-    return shipments[id]
+# @app.get("/shipment/{id}")
+# def get_shipment(id:int) -> dict[str, Any]:
+#
+#     if id not in shipments:
+#         return {"detail" : "Given id doesn't exist"}
+#
+#     return shipments[id]
 
 
-@app.put("/shipment")
-def put_shipment(id :int,content :str ,weight :float,status: str) -> dict[str ,Any]:
-    shipments[id] = {
-        "content": content,
-        "weight": weight,
-        "status" : status,
-    }
-    return shipments[id]
+# @app.put("/shipment")
+# def put_shipment(id :int,content :str ,weight :float,status: str) -> dict[str ,Any]:
+#     shipments[id] = {
+#         "content": content,
+#         "weight": weight,
+#         "status" : status,
+#     }
+#     return shipments[id]
 
 
 @app.patch("/shipment",response_model=ShipmentRead)
-def update_shipment(id:int,body : ShipmentUpdate) -> dict[str,Any]:
-
-    shipments[id].update(body.model_dump(exclude_none=True))
-    save()
-    return shipments[id]
+def update_shipment(id:int,shipment: ShipmentUpdate) -> dict[str,Any]:
+    shipment = db.update(id,shipment)
+    return shipment
 
 @app.delete("/shipment")
 def delete_shipment(id :int) -> dict[str ,str]:
-    shipments.pop(id)
+    db.delete(id)
     return {"detail": f"shipment #{id} was deleted"}
 
 
 
 
-# @app.get("/scalar",include_in_schema=False)
-# def get_scalar_docs():
-#     return get_scalar_api_reference(
-#         openapi_url=app.openapi_url,
-#         title="scalar API",
-#     )
+@app.get("/scalar",include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="scalar API",
+    )
